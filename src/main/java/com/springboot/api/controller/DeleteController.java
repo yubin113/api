1b3a255e1551bb2fbf77a9423f23c02f4d756be5
yubin <ydbqls13@naver.com>
2023-03-07 21:31:10 +0900

    deletecontroller 만듦

--- src/main/java/com/springboot/api/controller/DeleteController.java
+++ src/main/java/com/springboot/api/controller/DeleteController.java
@@ -1,10 +1,17 @@
 package com.springboot.api.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/delete-api")
 public class DeleteController {
-    
+    //http://localhost:8080/api/v1/delete-api/{String 값}
+    @DeleteMapping(value = "/{variable}")  //DeleteVariable에서 정의한 매개변수와 같은 매개변수 사용할 것
+    public String DeleteVariable(@PathVariable String variable){
+        return variable;
+    }
+
 }
